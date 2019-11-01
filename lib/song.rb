ce1b17a5422671eecb92da7b46c54d0fb308a12a
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-11-01 20:32:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,7 @@ class Song
     self.all.detect{|s| s.name == song_name}
   end
 
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
 end
