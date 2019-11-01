bf27716f4aa096a29176ecc77ae448d86ce87df3
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-11-01 20:42:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,4 +50,15 @@ class Song
     song.artist_name = artist_name
     song
   end
+
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 end
