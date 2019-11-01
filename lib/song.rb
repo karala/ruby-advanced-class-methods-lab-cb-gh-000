484174d0900a339246aabf1101d093f3b4f9219d
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-11-01 20:11:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,11 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.create
-    song = Song.new
-    song.create = name
-    @@all << song
-  end
-
   def self.all
     @@all
   end
 
-  def self.save
+  def save
     self.class.all << self
   end
 
